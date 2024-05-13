fe352dc3329f6970528a4168d91cd39dfae2e09c
ajfahim <ajfahim52@gmail.com>
2024-05-14 00:33:56 +0600

    small change

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,6 +4,10 @@ const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "25d",
   });
+  console.log(
+    "🚀 ~ generateTokenAndSetCookie ~ process.env.NODE_ENV:",
+    process.env.NODE_ENV
+  );
 
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000, // MS
