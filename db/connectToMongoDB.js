630dc44fcdc0795b6ab4848daf5261bdbba483a0
AlgoWebBot <shimulzahan636@gmail.com>
2024-05-21 22:51:53 +0600

    db set here

--- db/connectToMongoDB.js
+++ db/connectToMongoDB.js
@@ -3,8 +3,7 @@ const connectDB = async () => {
   try {
     mongoose.set("strictQuery", true);
     const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log(`🎯 MongoDB Connected: ${conn.connection.host}`);
+    console.log(`🎯 MongoDB Connected`);
   } catch (error) {
     console.log(error);
     process.exit(1);
